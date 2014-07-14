77a065f2a000cc1fb35ca939f4dcc0d9f11532ee
Jigar Revar <jigarrevar@Darshan-Revars-Mac-mini.local>
2014-07-14 08:55:08 -0700

    Initial Commit

---
+++ insultMaster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  insultMaster
+//
+//  Created by Jigar Revar on 7/14/14.
+//  Copyright (c) 2014 Jigar Revar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ insultMaster/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  insultMaster
+//
+//  Created by Jigar Revar on 7/14/14.
+//  Copyright (c) 2014 Jigar Revar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
